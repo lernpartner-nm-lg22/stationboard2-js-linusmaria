bc7316adb09a5464c9c878003e0cde294cc9313f
linusmaria <linus.hausammann@sbw-media.ch>
2024-10-21 15:30:36 +0200

    a

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,40 @@
 "use strict"
 // opendata api URL
-const url = `https://transport.opendata.ch/v1/stationboard?station=${station}`;
+const stationElement = document.getElementById("station");
+const buttonElement = document.getElementById("submit");
+const tableElement = document.getElementById("time-table");
 
-const getData = async () => {};
+buttonElement.addEventListener("click", async () => {
+    const data = await getData();
+    displayData(data);
+});
 
-const displayData = (data) => {};
+
+const getData = async () => {
+    const station = stationElement.value;
+    const url = `https://transport.opendata.ch/v1/stationboard?station=${station}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+};
+
+
+const displayData = (data) => {
+    const stationName = data.station.name;
+    const stationNameElement = document.getElementById("station-name");
+    stationNameElement.textContent = stationName;
+    data.stationboard.forEach((train) => {
+        const gleis = train.stop.platform;
+        const destination = train.to;
+        const time = new Date(train.stop.departure);
+        const timeString = time.toLocaleTimeString("de-Ch");
+
+        const row = tableElement.insertRow();
+        const cellUhrzeit = row.insertCell();
+        cellUhrzeit.textContent = timeString;
+        const cellZiel = row.insertCell();
+        cellZiel.textContent = destination;
+        const cellGleis = row.insertCell();
+        cellGleis.textContent = gleis;
+    })
+};
